575e79e7e42483895eda136a416130e89e26edca
Jonuts24 <23100174@slc-sflu.edu.ph>
2026-03-18 22:21:11 +0800

    side nav fixes

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -10,17 +10,17 @@ const SideNav = () => {
         <img src={robinsonsLogo} alt="Robinsons Malls" className="logo-side" />
       </div>
       <ul className="nav-links">
-        <li><NavLink to="/admin/dashboard" ><i className="fas fa-th-large"></i> Dashboard</NavLink></li>
-        <li><NavLink to="/admin/vouchers" ><i className="fas fa-tags"></i> Vouchers</NavLink></li>
-        <li><NavLink to="/admin/campaigns" ><i className="fas fa-bullhorn"></i> Campaigns</NavLink></li>
-        <li><NavLink to="/admin/claims" ><i className="fas fa-gift"></i> Claims</NavLink></li>
-        <li><NavLink to="/admin/transactions" ><i className="fas fa-history"></i> Transactions</NavLink></li>
-        <li><NavLink to="/admin/users" ><i className="fas fa-users"></i> Users</NavLink></li>
-        <li><NavLink to="/admin/reports" ><i className="fas fa-chart-bar"></i> Reports</NavLink></li>
-        <li><NavLink to="/admin/settings" ><i className="fas fa-cog"></i> Settings</NavLink></li>
+        <li><NavLink to="/admin/dashboard" ><i className="fa-solid fa-th-large"></i> Dashboard</NavLink></li>
+        <li><NavLink to="/admin/vouchers" ><i className="fa-solid fa-tags"></i> Vouchers</NavLink></li>
+        <li><NavLink to="/admin/campaigns" ><i className="fa-solid fa-volume-high"></i> Campaigns</NavLink></li>
+        <li><NavLink to="/admin/claims" ><i className="fa-solid fa-gift"></i> Claims</NavLink></li>
+        <li><NavLink to="/admin/transactions" ><i className="fa-solid fa-history"></i> Transactions</NavLink></li>
+        <li><NavLink to="/admin/users" ><i className="fa-solid fa-users"></i> Users</NavLink></li>
+        <li><NavLink to="/admin/reports" ><i className="fa-solid fa-chart-bar"></i> Reports</NavLink></li>
+        <li><NavLink to="/admin/settings" ><i className="fa-solid fa-cog"></i> Settings</NavLink></li>
       </ul>
       <div className="admin-user">
-        <div className="admin-icon"><i className="fas fa-user-circle"></i></div>
+        <div className="admin-icon"><i className="fa-solid fa-user-circle"></i></div>
         <div className="admin-info">
           <p>Admin User</p>
           <p>admin.user@gmail.com</p>
